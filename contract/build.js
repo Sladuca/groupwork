909df125b8b37ffcf193a4db1ebfb42357c1b8f3
ritatzhang <55301871+ritatzhang@users.noreply.github.com>
2020-05-23 12:32:12 -0400

    updating cargo

--- contract/build.js
+++ contract/build.js
@@ -1,8 +1,8 @@
-const shell = require('shelljs');
+const shell = require("shelljs");
 
 shell.fatal = true; // same as "set -e"
 
-shell.cd('contract');
+shell.cd("contract");
 // Note: see flags in ./cargo/config
-shell.exec('cargo build --target wasm32-unknown-unknown --release');
-shell.cp('./target/wasm32-unknown-unknown/release/status_message.wasm', './res');
+shell.exec("cargo build --target wasm32-unknown-unknown --release");
+shell.cp("./target/wasm32-unknown-unknown/release/groupwork.wasm", "./res");
