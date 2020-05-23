55f3800ccbdfdd87df4d5a9531c605cb927743aa
Sebastien La Duca <sladuca777@gmail.com>
2020-05-23 04:24:36 -0400

    update near-sdk

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,12 +1,12 @@
 use borsh::{BorshDeserialize, BorshSerialize};
-use near_sdk::{env, near_bindgen, payable, Balance};
+use near_sdk::{env, near_bindgen, Balance};
 use std::collections::{HashMap};
 
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 
-#[derive(Default, BorshDeserialize, BorshSerialize)]
+#[derive(Default, Clone, BorshDeserialize, BorshSerialize)]
 pub struct Group {
   stake_required: Balance,
   phase: u8, // 0, 1, 2
@@ -19,7 +19,7 @@ pub struct Group {
 #[derive(Default, BorshDeserialize, BorshSerialize)]
 pub struct GroupworkContract {
   counter: u64,
-  groups: HashMap<Balance, Group>
+  groups: HashMap<u64, Group>
 }
 
 #[near_bindgen]
@@ -28,8 +28,8 @@ impl GroupworkContract {
   #[payable]
   pub fn create_group(&mut self, stake_required: Balance) -> u64 {
     let mut stakes = HashMap::new();
-    stakes.insert(env::signer_account_id(), env::attached_depoit());
-    let mut reviews = HashMap::new();
+    stakes.insert(env::signer_account_id(), env::attached_deposit());
+    let reviews = HashMap::new();
     let group = Group {
       stake_required,
       phase: 0,
@@ -39,17 +39,18 @@ impl GroupworkContract {
     };
     self.counter += 1;
     self.groups.insert(self.counter, group);
+    self.counter
   }
 
   #[payable]
   pub fn join_group(&mut self, group_id: u64) -> Option<Group> {
-    match self.groups.get_mut(group_id) {
+    match self.groups.get_mut(&group_id) {
       Some(group) => {
         // update stakes
         group.stakes.insert(env::signer_account_id(), env::attached_deposit());
         // update members
-        group.members.push(value);
-        group
+        group.members.push(env::signer_account_id());
+        Some(group.clone())
       },
       None => {
         // TODO somehow return the funds to the caller
@@ -59,13 +60,13 @@ impl GroupworkContract {
   }
 
   pub fn get_group(&mut self, group_id: u64) -> Option<Group> {
-    match self.groups.get(group_id) {
+    match self.groups.get(&group_id) {
       Some(group) => { 
         // check if the signer is a memeber, return null if they arent
-        if (!group.members.contains(env::signer_account_id())) {
+        if !group.members.contains(&env::signer_account_id()) {
           return None
         }
-        group
+        Some(group.clone())
       },
       None => None
     }
